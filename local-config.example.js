b98b050329f8ea9e3153f0bb1225f37165f8b3b2
Christopher Davenport <9170087+42HLFGGK@users.noreply.github.com>
2026-05-07 10:42:49 -0500

    Remove local config example file

--- local-config.example.js
+++
@@ -1,7 +0,0 @@
-window.LOCAL_CONFIG = {
-  token: 'ODRkM2MxYTUtYzdiYi00NjcyLWI4NzEtNTE0MGU0Mjg1MDVjZjQ1Njg3ZGQtZGIx_PF84_183361eb-b803-4a69-8814-ae4432e025f7',
-  refreshMs: 30000,
-  searchUrl: 'https://api.wxcc-us1.cisco.com/search',
-  statusFilter: 'parked',
-  darkmode: false,
-}
